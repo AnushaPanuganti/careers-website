6a48b135ed809ce30728c0851a3a5987662caeae
AnushaPanuganti <107758299+AnushaPanuganti@users.noreply.github.com>
2024-07-13 14:59:06 +0000

    Added html and CSS

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
-app = Flask(__name__)   ##when u run this u will get __main__ as name variable has main value for app.py
+from flask import Flask, render_template
+
+app = Flask(
+    __name__
+)  ##when u run this u will get __main__ as name variable has main value for app.py
+
 
 @app.route("/")
 def hello_world():
-  return "Hello,World"
+  return render_template("home.html")
+
+
 if __name__ == "__main__":
-  app.run(host = "0.0.0.0",debug=True)
+  app.run(host="0.0.0.0", debug=True)
